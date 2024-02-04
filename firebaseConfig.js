75bcc67f2a0c497476747a5b552583b95c952c5d
Rishabh <rishabh@lorien.finance>
2024-02-04 13:12:22 +0530

    main

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBCWDyWWoSmkB0eZTJqVQr48uJvCRah9A4",
-  authDomain: "open-street-1b7ff.firebaseapp.com",
-  projectId: "open-street-1b7ff",
-  storageBucket: "open-street-1b7ff.appspot.com",
-  messagingSenderId: "777799235995",
-  appId: "1:777799235995:web:4cfc098775fcc9adbe493e",
-  measurementId: "G-HWX8W74NF9"
+  apiKey: 'AIzaSyBCWDyWWoSmkB0eZTJqVQr48uJvCRah9A4',
+  authDomain: 'open-street-1b7ff.firebaseapp.com',
+  projectId: 'open-street-1b7ff',
+  storageBucket: 'open-street-1b7ff.appspot.com',
+  messagingSenderId: '777799235995',
+  appId: '1:777799235995:web:4cfc098775fcc9adbe493e',
+  measurementId: 'G-HWX8W74NF9'
 };
 
 const app = initializeApp(firebaseConfig);
